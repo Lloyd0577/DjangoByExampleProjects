c970aabd1d5a2050042b8b086a79c14cdb4a0199
lloyd <651519531@qq.com>
2017-11-07 22:36:11 +0800

    social web

---
+++ SocialWeb/env/bookmarks/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/lloyd/python/DjangoProjects/DjangoByExampleProjects/SocialWeb/env/bookmarks/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
