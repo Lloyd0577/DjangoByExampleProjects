6a660b65049e8005b28e0f4d735fbc197e5dea4a
lloyd <651519531@qq.com>
2017-11-17 22:55:25 +0800

    5chapter

--- SocialWeb/bookmarks/bookmarks/settings.py
+++ SocialWeb/bookmarks/bookmarks/settings.py
@@ -41,7 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.admin',
-    'social.apps.django_app.default',
+ #   'social.apps.django_app.default',
+    'images',
 
 ]
 
